ec9b0dbb1047971f847f281ff3a16da3cac01c95
gokilaunitd <gokila@unitdtechnologies.com>
2024-03-11 13:54:23 +0530

    first commmit

--- src/constants/api.js
+++ src/constants/api.js
@@ -16,7 +16,7 @@ import axios from 'axios';
 // });
 
 const api = axios.create({
-  baseURL: 'http://43.228.126.245:3001',
+  baseURL: 'http://43.228.126.245:4021',
  // baseURL: 'http://localhost:6001',
   
   
